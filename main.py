820ec7fa2437461128496430adc92b54446adba7
Luiz Cabral <lcabral@gmail..com>
2023-06-09 10:30:28 -0300

    Implementação

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
     
-@app.route("/about)
-def about():
-    return render_template("about.html")
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
